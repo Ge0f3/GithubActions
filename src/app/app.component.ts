756382ba16b2c76c6fef6633e848842f24ce2cc5
Geoffrey <geoffrey.geofe@gmail.com>
2019-09-19 01:42:21 -0500

    simple angular app

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'GithubActions';
+
+  greet(){
+    alert("welcome to github actions");
+  }
+}
